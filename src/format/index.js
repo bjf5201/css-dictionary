0844e711ca1fab592a0506f8da9849d35bb98a63
Bethany Fannin <contact@bethanyfannin.com>
2024-08-04 19:00:33 -0400

    Create library of functions to format the selector output

---
+++ src/format/index.js
@@ -0,0 +1,91 @@
+const LINKS = {
+  nth: 'https://css-tricks.com/examples/nth-child-tester/',
+}
+
+const capitalise = value => value.slice(0, 1).toUpperCase() + value.slice(1)
+
+const asSentence = value => capitalise(value) + '.'
+
+export const linkify = format => (label, href) => {
+  if (format === 'html') {
+    return `<a href="${href}" rel="noopener noreferrer" target="_blank">${label}</a>`
+  }
+
+  if (format === 'markdown') {
+    return `[${label}](${href})`
+  }
+
+  return label
+}
+
+export const addLineBreaks = format => value => {
+  const cr = format === 'html' ? '<br>' : format === 'markdown' ? '  \n' : '\n'
+
+  return value
+    .replace(/([^yf]) within/g, (...args) => `${args[1]}${cr}… within`)
+    .replace(/([^y]) after/g, (...args) => `${args[1]}${cr}… after`)
+    .replace(/([^f]) directly/g, (...args) => `${args[1]}${cr}… directly`)
+    .replace(/element of/g, `element${cr}… of`)
+    .replace(/itself/g, `${cr}… itself`)
+}
+
+const toHTML = (value, options) => {
+  let result = asSentence(value)
+    // Make sure tag names are properly displayed and not rendered as HTML
+    .replace(/</g, '&lt;')
+    .replace(/>/g, '&gt;')
+    // Wrap emphases with <code> elements for clarity
+    .replace(/‘([^’]+)’/g, (...args) => `<code>${args[1]}</code>`)
+
+  // Insert line breaks for readability
+  if (options.lineBreaks) {
+    result = addLineBreaks(options.format)(result)
+  }
+
+  // Add useful links
+  if (options.links) {
+    const link = linkify(options.format)
+
+    result = result
+      .replace(/nth child/g, link('nth child', LINKS.nth))
+      .replace(/nth of its type/g, link('nth of its type', LINKS.nth))
+  }
+
+  return result
+}
+
+const toMarkdown = (value, options) => {
+  let result = asSentence(value)
+    .replace(/[‘’]/g, '`')
+    // Make sure tag names are properly displayed and not rendered as HTML
+    .replace(/</g, '&lt;')
+    .replace(/>/g, '&gt;')
+
+  // Insert line breaks for readability
+  if (options.lineBreaks) {
+    result = addLineBreaks(options.format)(result)
+  }
+
+  // Add useful links
+  if (options.links) {
+    const link = linkify(options.format)
+
+    result = result
+      .replace(/nth child/g, link('nth child', LINKS.nth))
+      .replace(/nth of its type/g, link('nth of its type', LINKS.nth))
+  }
+
+  return result
+}
+
+export default (options = {}) => selector => {
+  switch (options.format) {
+    case 'html':
+      return toHTML(selector, options)
+    case 'markdown':
+      return toMarkdown(selector, options)
+    case 'raw':
+    default:
+      return asSentence(selector)
+  }
+}
---
+++ src/format/spec.js
@@ -0,0 +1,47 @@
+import format, { linkify, addLineBreaks } from './'
+
+describe('The `format` helper', () => {
+  it('should handle markdown format', () => {
+    expect(format({ format: 'markdown' })('an ‘<a>’ element')).toBe(
+      'An `&lt;a&gt;` element.'
+    )
+  })
+
+  it('should handle HTML format', () => {
+    expect(format({ format: 'html' })('an ‘<a>’ element')).toBe(
+      'An <code>&lt;a&gt;</code> element.'
+    )
+  })
+})
+
+describe('The `linkify` function', () => {
+  it('should handle HTML links', () => {
+    expect(linkify('html')('Kitty', 'https://kittygiraudel.com')).toBe(
+      '<a href="https://kittygiraudel.com" rel="noopener noreferrer" target="_blank">Kitty</a>'
+    )
+  })
+
+  it('should handle Markdown links', () => {
+    expect(linkify('markdown')('Kitty', 'https://kittygiraudel.com')).toBe(
+      '[Kitty](https://kittygiraudel.com)'
+    )
+  })
+
+  it('should not output links in raw format', () => {
+    expect(linkify('raw')('Kitty', 'https://kittygiraudel.com')).toBe('Kitty')
+  })
+})
+
+describe('The `addLineBreaks` function', () => {
+  it('should handle HTML line breaks', () => {
+    expect(addLineBreaks('html')('itself')).toBe('<br>… itself')
+  })
+
+  it('should handle Markdown line breaks', () => {
+    expect(addLineBreaks('markdown')('itself')).toBe('  \n… itself')
+  })
+
+  it('should handle raw line breaks', () => {
+    expect(addLineBreaks('raw')('itself')).toBe('\n… itself')
+  })
+})
